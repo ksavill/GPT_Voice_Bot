c21500d27c9cf430c5d19e2d39f0fac6935c5f99
Kevin Savill <103211354+ksavill@users.noreply.github.com>
2023-04-09 16:16:19 -0500

    4-9-2023

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 # import openai
-import speech_recognition
-import pyttsx3
+
 import pyaudio
 import requests
 import json
 from dearpygui import dearpygui as dpg
+
 import sys
+import threading
+import re
+import speech_recognition
+import pyttsx3
+import datetime
 
 # Initialize OpenAI API
 api_key = "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"
+speaking = False
+
+callingName = "Vega"
+selectedModel = "gpt-3.5-turbo"
+immediateQuestion = ""
+globalWidth = 800
+globalHeight = 800
 
 # Initialize the text to speech engine
 engine = pyttsx3.init()
@@ -24,6 +36,28 @@ def transcribe_audio_to_text(filename):
     except:
         print("Skipping unknown error")
 
+def offline_response(text):
+
+    # turn off mic listening
+    if text == "stop listening":
+        dpg.set_value("allow_microphone_input", False)
+        return "Turning off microphone input"
+    
+    # get the current date
+    if text == "what date is it" or text == "what is the date":
+        date = datetime.datetime.now()
+        formatted_date = date.strftime("%A, %B %d, %Y")
+        return f"Today is {formatted_date}"
+    
+    # get the current time
+    if text == "what time is it" or text == "what is the time":
+        time = datetime.datetime.now()
+        formatted_time = time.strftime("%I:%M %p")
+        return f"The current time is {formatted_time}"
+    
+
+    # catch all
+    return ""
 
 def generate_response(prompt):
     global conversation_history
@@ -53,18 +87,106 @@ def generate_response(prompt):
     else:
         raise Exception(f"An error occurred: {response_json}")
 
+def extract_code_and_text(text):
+    code_blocks = re.findall(r'```(.*?)```', text, re.DOTALL)
+    non_code_text = re.sub(r'```(.*?)```', '', text, flags=re.DOTALL).strip()
+
+    return non_code_text, code_blocks
+
 def speak_text(text):
-    engine.say(text)
-    engine.runAndWait()
+    global speaking, immediateQuestion
+    dpg.set_value("last_response", text)
+    speaking = True
+    stop_listener_thread = threading.Thread(target=listen_for_stop)
+    stop_listener_thread.start()
+
+    non_code_text, code_blocks = extract_code_and_text(text)
+
+    # Print the code blocks to the console
+    if code_blocks:
+        print("\nCode output detected. I will not read it out loud, but here it is:\n")
+        for code_block in code_blocks:
+            print(f"{code_block.strip()}\n")
+
+    # Split the non-code text into words and create a list to store blocks of 10 words max
+    words = non_code_text.split()
+    text_blocks = []
+
+    # Break the words into blocks of 10 words max
+    for i in range(0, len(words), 8):
+        text_block = " ".join(words[i:i+8])
+        text_blocks.append(text_block)
+
+    # Call engine.say() for each block of words in the list
+    for text_block in text_blocks:
+        if speaking == True:
+            engine.say(text_block)
+            engine.runAndWait()
+        else:
+            break
+
+    speaking = False
+    stop_listener_thread.join()
+
+    if immediateQuestion:
+        request_text = immediateQuestion
+        immediateQuestion = ""
+        process_request(request_text)
+
+def listen_for_stop():
+    global speaking, immediateQuestion
+    print("listening for stop")
+    stopSaid = False
+    while speaking and dpg.get_value("allow_microphone_input"):
+        with speech_recognition.Microphone() as source:
+            recognizer = speech_recognition.Recognizer()
+            recognizer.pause_threshold = 0.5
+            try:
+                audio = recognizer.listen(source, timeout=1)
+                transcription = recognizer.recognize_google(audio)
+                print(f"You said: {transcription}")
+                if "stop" in transcription.lower():
+                    speaking = False
+                    stopSaid = True
+                    print("stopping text output")
+                    break
+            except speech_recognition.WaitTimeoutError:
+                pass
+            except Exception as e:
+                print("An error occurred: {}".format(e))
+    print("no longer listening for stop")
+    if stopSaid == True and len(transcription.lower()) > 4:
+        print("placeholder for stop and another likely question has been spoken.")
+        text = transcription.lower()
+        immediateQuestion = text.replace("stop", "", 1).strip()
+
+def process_request(request_text):
+    # update you_said
+    dpg.set_value("you_said", request_text)
+    # offline input checks
+    response = offline_response(request_text)
+    if response:
+        speak_text(response)
+        return
+    
+    # openai gpt 
+    response = generate_response(request_text)
+    print(f"GPT says: {response}")
+    if response:
+        speak_text(response)
+        return
+    
+    # catch-all speak text.
+    response = "No valid response given."
+    speak_text(response)
 
 def record_question():
     if dpg.get_value("allow_microphone_input") == False:
         return
-    speak_text("Say vega to start recording your question")
-    print("Say 'vega' to start recording your question")
+    speak_text(f"Say {callingName} to ask your question")
+    print(f"Say {callingName} to ask your question")
     global conversation_history
     while dpg.get_value("allow_microphone_input"):
-        # Wait for user to say "vega"
         
         with speech_recognition.Microphone() as source:
             recognizer = speech_recognition.Recognizer()
@@ -72,10 +194,8 @@ def record_question():
             try:
                 transcription = recognizer.recognize_google(audio)
                 print(f'You said: {transcription}')
-                if "vega" in transcription.lower():
+                if callingName.lower() in transcription.lower():
                     while dpg.get_value("allow_microphone_input"):
-                        # Record audio
-                        filename = "input.wav"
                         speak_text("Say your question")
                         print("Say your question (or 'end' to stop asking)")
                         with speech_recognition.Microphone() as source:
@@ -84,19 +204,16 @@ def record_question():
                             audio = recognizer.listen(source, timeout=30)
                             try:
                                 text = recognizer.recognize_google(audio)
-                                if text.lower() == "end conversation":
+                                text = text.lower()
+                                if text == "stop conversation":
+                                    speak_text("Breaking from thead.")
                                     break
 
                                 # Check if there are more than 2 words
                                 if len(text.split()) >= 2:
                                     print(f"You said: {text}")
 
-                                    # Generate the response
-                                    response = generate_response(text)
-                                    print(f"GPT says: {response}")
-
-                                    # Speak the response using text-to-speech
-                                    speak_text(response)
+                                    process_request(text)
                                     speak_text("now follow-up")
                                 else:
                                     speak_text("Not enough words.")
@@ -119,16 +236,43 @@ def record_question():
     speak_text("Voice listening ended.")
     print("Voice listening ended.")
 
+def UpdateCallingName():
+    global callingName
+    inputName = dpg.get_value("callingName")
+    if len(inputName) > 2:
+        callingName = dpg.get_value("callingName")
+        speak_text(f"Wake word updated to {callingName}")
+    else:
+        speak_text("Invalid name input.")
+
+def UpdateSelectedModel():
+    global selectedModel
+    selectedModel = dpg.get_value("selectedModel")
+    speak_text(f"Selected Model has been updated to {selectedModel}")
+
 def main():
-    
+    global callingName
+
     # Create the GUI
-    with dpg.window(label="Voice Assistant", width=250, height=250):
-        dpg.add_checkbox(label="Allow microphone input", default_value=False, id="allow_microphone_input", callback=record_question)
+    with dpg.window(label="Voice Assistant", width=globalWidth, height=globalHeight):
+        dpg.add_checkbox(label="Allow microphone input", default_value=False, tag="allow_microphone_input", callback=record_question)
+        with dpg.group(horizontal=True):
+            dpg.add_text("Calling word: ")
+            dpg.add_input_text(tag="callingName", default_value=callingName, width=100, on_enter=True, callback=UpdateCallingName)
+            dpg.add_button(label="Update Calling Name", callback=UpdateCallingName)
+        dpg.add_text("Select Chat Model")
+        dpg.add_radio_button(['gpt-3.5-turbo','whisper-1','text-davinci-003'], tag="selectedModel", default_value="gpt-3.5-turbo", callback=UpdateSelectedModel)
+    
+        dpg.add_text("You said:")
+        dpg.add_text(tag="you_said")
+        dpg.add_text("\n\n")
+        dpg.add_text("Last response:")
+        dpg.add_text(tag="last_response")
     
 
 if __name__ == "__main__":
     dpg.create_context()
-    dpg.create_viewport(title="GPT Voice Assistant GUI", width=-1, height=-1)
+    dpg.create_viewport(title="GPT Voice Assistant GUI", width=globalWidth, height=globalHeight)
     dpg.setup_dearpygui()
 
 
